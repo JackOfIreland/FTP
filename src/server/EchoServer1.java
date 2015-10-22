b9b17663a58c859cfa290808dd860ffe0f362053
unknown <t00168584@1XX3417EBA7A07B.students.ittralee.local>
2015-10-22 15:21:24 +0100

    Initial commit

---
+++ src/server/EchoServer1.java
@@ -0,0 +1,55 @@
+package server;
+
+import java.io.*;
+
+/**
+ * This module contains the application logic of an echo server
+ * which uses a connectionless datagram socket for interprocess 
+ * communication.
+ * A command-line argument is required to specify the server port.
+ * @author M. L. Liu
+ */
+
+public class EchoServer1 {
+   public static void main(String[] args) {
+      int serverPort = 7;    // default port
+      if (args.length == 1 )
+         serverPort = Integer.parseInt(args[0]);       
+      try {
+         // instantiates a datagram socket for both sending
+         // and receiving data
+   	   MyServerDatagramSocket mySocket = new MyServerDatagramSocket(serverPort); 
+         System.out.println("Echo server ready.");  
+         while (true) {  // forever loop
+            DatagramMessage request = 
+               mySocket.receiveMessageAndSender();
+            System.out.println("Request received");
+            String message = request.getMessage( );
+            System.out.println("message received: "+ message);
+
+            checkForDirectory(message);
+
+            // Now send the echo to the requestor
+            mySocket.sendMessage(request.getAddress( ),
+               request.getPort( ), message);
+		   } //end while
+       } // end try
+	    catch (Exception ex) {
+          ex.printStackTrace( );
+	    } // end catch
+   } //end main
+
+   private static void checkForDirectory(String message) {
+      File f = new File("C:\\Users\\t00168584\\Desktop\\"+ message.trim());
+      if (f.exists())
+      {
+         System.out.println("Welcome " + message.trim());
+      }
+      else
+      {
+         System.out.println("Creating directory called " + message.trim());
+         f.mkdir();
+
+      }
+   }
+} // end class      
---
+++ src/server/asdasd.java
@@ -0,0 +1,10 @@
+package server;
+
+/**
+ * Created by t00168584 on 22/10/2015.
+ */
+public class asdasd {
+    public static void main(String[] args) {
+
+    }
+}
